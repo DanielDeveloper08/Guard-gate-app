3a4f8361b5bcca20cfa0852854902a0d01870634
DanielDeveloper08 <danielprogramando2000@gmail.com>
2023-12-05 00:21:07 -0500

    fix counter in panic button

--- src/app/modules/panic-alert/panic-alert.component.ts
+++ src/app/modules/panic-alert/panic-alert.component.ts
@@ -4,31 +4,30 @@ import { IonModal, ModalController } from '@ionic/angular';
 @Component({
   selector: 'app-panic-alert',
   templateUrl: './panic-alert.component.html',
-  styleUrls: ['./panic-alert.component.scss']
+  styleUrls: ['./panic-alert.component.scss'],
 })
-export class PanicAlertComponent{
+export class PanicAlertComponent {
   counter: number = 0;
   @ViewChild('modal') modal!: IonModal;
   timer: any;
 
-  onButtonPress(): void {
+  async onButtonPress(): Promise<void> {
     this.timer = setInterval(() => {
-      if(this.counter <= 4){
+      if (this.counter <= 4) {
         this.counter++;
-      }
-  
-      if(this.counter === 5){
-        this.modal.present();
-        // this.counter = 0;
       }
     }, 1000);
 
-
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    if (this.counter == 5) {
+      this.modal.present();
+      clearInterval(this.timer);
+      this.counter = 0;
+    }
   }
 
   onButtonRelease(): void {
     clearInterval(this.timer);
     this.counter = 0;
   }
-
 }
